f41f2903bf8dbc7ff100ea59ae8535ff2ce56159
jermp <jeis90@gmail.com>
2022-06-26 16:40:02 +0200

    check correctness of lookup_advanced

--- include/dictionary.cpp
+++ include/dictionary.cpp
@@ -58,68 +58,10 @@ contig_query_result dictionary::lookup_uint64_canonical_parsing(uint64_t uint64_
     return m_buckets.lookup_canonical<advanced>(begin, end, uint64_kmer, uint64_kmer_rc, m_k, m_m);
 }
 
-// uint64_t dictionary::lookup_uint64_regular_parsing(uint64_t uint64_kmer) const {
-//     uint64_t minimizer = util::compute_minimizer(uint64_kmer, m_k, m_m, m_seed);
-//     uint64_t bucket_id = m_minimizers.lookup(minimizer);
-
-//     if (m_skew_index.empty()) return m_buckets.lookup(bucket_id, uint64_kmer, m_k, m_m);
-
-//     auto [begin, end] = m_buckets.locate_bucket(bucket_id);
-//     uint64_t num_super_kmers_in_bucket = end - begin;
-//     uint64_t log2_bucket_size = util::ceil_log2_uint32(num_super_kmers_in_bucket);
-//     if (log2_bucket_size > m_skew_index.min_log2) {
-//         uint64_t pos = m_skew_index.lookup(uint64_kmer, log2_bucket_size);
-//         /* It must hold pos < num_super_kmers_in_bucket for the kmer to exist. */
-//         if (pos < num_super_kmers_in_bucket) {
-//             return m_buckets.lookup_in_super_kmer(begin + pos, uint64_kmer, m_k, m_m);
-//         }
-//         return constants::invalid_uint64;
-//     }
-
-//     return m_buckets.lookup(begin, end, uint64_kmer, m_k, m_m);
-// }
-
-// uint64_t dictionary::lookup_uint64_canonical_parsing(uint64_t uint64_kmer) const {
-//     uint64_t uint64_kmer_rc = util::compute_reverse_complement(uint64_kmer, m_k);
-//     uint64_t minimizer = util::compute_minimizer(uint64_kmer, m_k, m_m, m_seed);
-//     uint64_t minimizer_rc = util::compute_minimizer(uint64_kmer_rc, m_k, m_m, m_seed);
-//     uint64_t bucket_id = m_minimizers.lookup(std::min<uint64_t>(minimizer, minimizer_rc));
-
-//     if (m_skew_index.empty()) {
-//         return m_buckets.lookup_canonical(bucket_id, uint64_kmer, uint64_kmer_rc, m_k, m_m);
-//     }
-
-//     auto [begin, end] = m_buckets.locate_bucket(bucket_id);
-//     uint64_t num_super_kmers_in_bucket = end - begin;
-//     uint64_t log2_bucket_size = util::ceil_log2_uint32(num_super_kmers_in_bucket);
-//     if (log2_bucket_size > m_skew_index.min_log2) {
-//         uint64_t pos = m_skew_index.lookup(uint64_kmer, log2_bucket_size);
-//         if (pos < num_super_kmers_in_bucket) {
-//             uint64_t kmer_id = m_buckets.lookup_in_super_kmer(begin + pos, uint64_kmer, m_k,
-//             m_m); if (kmer_id != constants::invalid_uint64) return kmer_id;
-//         }
-//         uint64_t pos_rc = m_skew_index.lookup(uint64_kmer_rc, log2_bucket_size);
-//         if (pos_rc < num_super_kmers_in_bucket) {
-//             uint64_t kmer_id =
-//                 m_buckets.lookup_in_super_kmer(begin + pos_rc, uint64_kmer_rc, m_k, m_m);
-//             return kmer_id;
-//         }
-//         return constants::invalid_uint64;
-//     }
-//     return m_buckets.lookup_canonical(begin, end, uint64_kmer, uint64_kmer_rc, m_k, m_m);
-// }
-
 uint64_t dictionary::lookup(char const* string_kmer, bool check_reverse_complement_too) const {
     uint64_t uint64_kmer = util::string_to_uint64_no_reverse(string_kmer, m_k);
     return lookup_uint64(uint64_kmer, check_reverse_complement_too);
 }
-
-contig_query_result dictionary::lookup_advanced(char const* string_kmer,
-                                                bool check_reverse_complement_too) const {
-    uint64_t uint64_kmer = util::string_to_uint64_no_reverse(string_kmer, m_k);
-    return lookup_advanced_uint64(uint64_kmer, check_reverse_complement_too);
-}
-
 uint64_t dictionary::lookup_uint64(uint64_t uint64_kmer, bool check_reverse_complement_too) const {
     constexpr bool advanced = false;
     if (m_canonical_parsing) {
@@ -134,6 +76,11 @@ uint64_t dictionary::lookup_uint64(uint64_t uint64_kmer, bool check_reverse_comp
     return res.kmer_id;
 }
 
+contig_query_result dictionary::lookup_advanced(char const* string_kmer,
+                                                bool check_reverse_complement_too) const {
+    uint64_t uint64_kmer = util::string_to_uint64_no_reverse(string_kmer, m_k);
+    return lookup_advanced_uint64(uint64_kmer, check_reverse_complement_too);
+}
 contig_query_result dictionary::lookup_advanced_uint64(uint64_t uint64_kmer,
                                                        bool check_reverse_complement_too) const {
     constexpr bool advanced = true;
@@ -151,7 +98,6 @@ contig_query_result dictionary::lookup_advanced_uint64(uint64_t uint64_kmer,
 bool dictionary::is_member(char const* string_kmer, bool check_reverse_complement_too) const {
     return lookup(string_kmer, check_reverse_complement_too) != constants::invalid_uint64;
 }
-
 bool dictionary::is_member_uint64(uint64_t uint64_kmer, bool check_reverse_complement_too) const {
     return lookup_uint64(uint64_kmer, check_reverse_complement_too) != constants::invalid_uint64;
 }
@@ -171,6 +117,7 @@ uint64_t dictionary::contig_size(uint64_t contig_id) const {
     assert(contig_length >= m_k);
     return contig_length - m_k + 1;
 }
+
 // std::vector<uint32_t> contig_neighbours(uint64_t contig_id) const;
 
 uint64_t dictionary::num_bits() const {
--- src/check_utils.hpp
+++ src/check_utils.hpp
@@ -12,6 +12,8 @@ bool check_correctness_lookup_access(std::istream& is, dictionary const& dict) {
     std::string line;
     uint64_t pos = 0;
     uint64_t num_kmers = 0;
+    contig_query_result prev;
+    prev.contig_id = 0;
 
     std::string got_kmer_str(k, 0);
     std::string expected_kmer_str(k, 0);
@@ -27,6 +29,7 @@ bool check_correctness_lookup_access(std::istream& is, dictionary const& dict) {
         for (uint64_t i = 0; i + k <= line.size(); ++i) {
             assert(util::is_valid(line.data() + i, k));
             uint64_t uint64_kmer = util::string_to_uint64_no_reverse(line.data() + i, k);
+            bool orientation = constants::forward_orientation;
 
             if (num_kmers != 0 and num_kmers % 5000000 == 0) {
                 std::cout << "checked " << num_kmers << " kmers" << std::endl;
@@ -34,6 +37,7 @@ bool check_correctness_lookup_access(std::istream& is, dictionary const& dict) {
             if ((num_kmers & 1) == 0) {
                 /* transform 50% of the kmers into their reverse complements */
                 uint64_kmer = util::compute_reverse_complement(uint64_kmer, k);
+                orientation = constants::backward_orientation;
             }
             util::uint64_to_string_no_reverse(uint64_kmer, expected_kmer_str.data(), k);
             uint64_t id = dict.lookup(expected_kmer_str.c_str());
@@ -48,6 +52,74 @@ bool check_correctness_lookup_access(std::istream& is, dictionary const& dict) {
                 std::cout << "kmer '" << expected_kmer_str << "' not found!" << std::endl;
             }
             assert(id != constants::invalid_uint64);
+
+            auto curr = dict.lookup_advanced(expected_kmer_str.c_str());
+            assert(curr.kmer_id == id);
+
+            if (curr.kmer_orientation != orientation) {
+                std::cout << "ERROR: got orientation " << int(curr.kmer_orientation)
+                          << " but expected " << int(orientation) << std::endl;
+            }
+            assert(curr.kmer_orientation == orientation);
+
+            if (num_kmers == 0) {
+                if (curr.contig_id != 0) {
+                    std::cout << "contig_id " << curr.contig_id << " but expected 0" << std::endl;
+                }
+                assert(curr.contig_id == 0);  // at the beginning, contig_id must be 0
+            } else {
+                if (curr.kmer_id != prev.kmer_id + 1) {
+                    std::cout << "ERROR: got curr.kmer_id " << curr.kmer_id << " but expected "
+                              << prev.kmer_id + 1 << std::endl;
+                }
+                assert(curr.kmer_id == prev.kmer_id + 1);  // kmer_id must be sequential
+
+                if (curr.kmer_id_in_contig >= curr.contig_size) {
+                    std::cout << "ERROR: got curr.kmer_id_in_contig " << curr.kmer_id_in_contig
+                              << " but expected something < " << curr.contig_size << std::endl;
+                }
+                assert(curr.kmer_id_in_contig <
+                       curr.contig_size);  // kmer_id_in_contig must always be < contig_size
+
+                if (curr.contig_id == prev.contig_id) {
+                    /* same contig */
+                    if (curr.contig_size != prev.contig_size) {
+                        std::cout << "ERROR: got curr.contig_size " << curr.contig_size
+                                  << " but expected " << prev.contig_size << std::endl;
+                    }
+                    assert(curr.contig_size == prev.contig_size);  // contig_size must be same
+                    if (curr.kmer_id_in_contig != prev.kmer_id_in_contig + 1) {
+                        std::cout << "ERROR: got curr.kmer_id_in_contig " << curr.kmer_id_in_contig
+                                  << " but expected " << prev.kmer_id_in_contig + 1 << std::endl;
+                    }
+                    assert(curr.kmer_id_in_contig ==
+                           prev.kmer_id_in_contig + 1);  // kmer_id_in_contig must be sequential
+                } else {
+                    /* we have changed contig */
+                    if (curr.contig_id != prev.contig_id + 1) {
+                        std::cout << "ERROR: got curr.contig_id " << curr.contig_id
+                                  << " but expected " << prev.contig_id + 1 << std::endl;
+                    }
+                    assert(curr.contig_id ==
+                           prev.contig_id + 1);  // contig_id must be sequential since we stream
+                    if (curr.kmer_id_in_contig != 0) {
+                        std::cout << "ERROR: got curr.kmer_id_in_contig " << curr.kmer_id_in_contig
+                                  << " but expected 0" << std::endl;
+                    }
+                    assert(curr.kmer_id_in_contig ==
+                           0);  // kmer_id_in_contig must be 0 when we change contig
+                }
+            }
+
+            /* check also contig_size() */
+            uint64_t contig_size = dict.contig_size(curr.contig_id);
+            if (contig_size != curr.contig_size) {
+                std::cout << "ERROR: got contig_size " << contig_size << " but expected "
+                          << curr.contig_size << std::endl;
+            }
+            assert(contig_size == curr.contig_size);
+
+            prev = curr;
 
             // check access
             dict.access(id, got_kmer_str.data());
