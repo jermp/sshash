5902989298f62e682d166b251234649c090425f1
Giulio Ermanno Pibiri <jeis90@gmail.com>
2023-02-07 15:03:02 +0100

    added benchmarks log

--- script/bench.sh
+++ script/bench.sh
@@ -2,36 +2,36 @@
 
 ### regular indexes
 
-./sshash bench -i celegans.k31.sshash >> sshash.benchmarking_log
-./sshash bench -i celegans.k47.sshash >> sshash.benchmarking_log
-./sshash bench -i celegans.k63.sshash >> sshash.benchmarking_log
+./sshash bench -i celegans.k31.sshash >> sshash.regular.bench_log
+./sshash bench -i celegans.k47.sshash >> sshash.regular.bench_log
+./sshash bench -i celegans.k63.sshash >> sshash.regular.bench_log
 
-./sshash bench -i cod.k31.sshash >> sshash.benchmarking_log
-./sshash bench -i cod.k47.sshash >> sshash.benchmarking_log
-./sshash bench -i cod.k63.sshash >> sshash.benchmarking_log
+./sshash bench -i cod.k31.sshash >> sshash.regular.bench_log
+./sshash bench -i cod.k47.sshash >> sshash.regular.bench_log
+./sshash bench -i cod.k63.sshash >> sshash.regular.bench_log
 
-./sshash bench -i kestrel.k31.sshash >> sshash.benchmarking_log
-./sshash bench -i kestrel.k47.sshash >> sshash.benchmarking_log
-./sshash bench -i kestrel.k63.sshash >> sshash.benchmarking_log
+./sshash bench -i kestrel.k31.sshash >> sshash.regular.bench_log
+./sshash bench -i kestrel.k47.sshash >> sshash.regular.bench_log
+./sshash bench -i kestrel.k63.sshash >> sshash.regular.bench_log
 
-./sshash bench -i human.k31.sshash >> sshash.benchmarking_log
-./sshash bench -i human.k47.sshash >> sshash.benchmarking_log
-./sshash bench -i human.k63.sshash >> sshash.benchmarking_log
+./sshash bench -i human.k31.sshash >> sshash.regular.bench_log
+./sshash bench -i human.k47.sshash >> sshash.regular.bench_log
+./sshash bench -i human.k63.sshash >> sshash.regular.bench_log
 
 ### canonical indexes
 
-./sshash bench -i celegans.k31.canon.sshash >> sshash.benchmarking_log
-./sshash bench -i celegans.k47.canon.sshash >> sshash.benchmarking_log
-./sshash bench -i celegans.k63.canon.sshash >> sshash.benchmarking_log
+./sshash bench -i celegans.k31.canon.sshash >> sshash.canon.bench_log
+./sshash bench -i celegans.k47.canon.sshash >> sshash.canon.bench_log
+./sshash bench -i celegans.k63.canon.sshash >> sshash.canon.bench_log
 
-./sshash bench -i cod.k31.canon.sshash >> sshash.benchmarking_log
-./sshash bench -i cod.k47.canon.sshash >> sshash.benchmarking_log
-./sshash bench -i cod.k63.canon.sshash >> sshash.benchmarking_log
+./sshash bench -i cod.k31.canon.sshash >> sshash.canon.bench_log
+./sshash bench -i cod.k47.canon.sshash >> sshash.canon.bench_log
+./sshash bench -i cod.k63.canon.sshash >> sshash.canon.bench_log
 
-./sshash bench -i kestrel.k31.canon.sshash >> sshash.benchmarking_log
-./sshash bench -i kestrel.k47.canon.sshash >> sshash.benchmarking_log
-./sshash bench -i kestrel.k63.canon.sshash >> sshash.benchmarking_log
+./sshash bench -i kestrel.k31.canon.sshash >> sshash.canon.bench_log
+./sshash bench -i kestrel.k47.canon.sshash >> sshash.canon.bench_log
+./sshash bench -i kestrel.k63.canon.sshash >> sshash.canon.bench_log
 
-./sshash bench -i human.k31.canon.sshash >> sshash.benchmarking_log
-./sshash bench -i human.k47.canon.sshash >> sshash.benchmarking_log
-./sshash bench -i human.k63.canon.sshash >> sshash.benchmarking_log
+./sshash bench -i human.k31.canon.sshash >> sshash.canon.bench_log
+./sshash bench -i human.k47.canon.sshash >> sshash.canon.bench_log
+./sshash bench -i human.k63.canon.sshash >> sshash.canon.bench_log
