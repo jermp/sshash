cc99af108999e6c845151d761eec44b3d1448ab9
Giulio Ermanno Pibiri <jeis90@gmail.com>
2023-02-07 14:55:54 +0100

    added script

--- script/bench.sh
+++ script/bench.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+### regular indexes
+
 ./sshash bench -i celegans.k31.sshash >> sshash.benchmarking_log
 ./sshash bench -i celegans.k47.sshash >> sshash.benchmarking_log
 ./sshash bench -i celegans.k63.sshash >> sshash.benchmarking_log
@@ -15,3 +17,21 @@
 ./sshash bench -i human.k31.sshash >> sshash.benchmarking_log
 ./sshash bench -i human.k47.sshash >> sshash.benchmarking_log
 ./sshash bench -i human.k63.sshash >> sshash.benchmarking_log
+
+### canonical indexes
+
+./sshash bench -i celegans.k31.canon.sshash >> sshash.benchmarking_log
+./sshash bench -i celegans.k47.canon.sshash >> sshash.benchmarking_log
+./sshash bench -i celegans.k63.canon.sshash >> sshash.benchmarking_log
+
+./sshash bench -i cod.k31.canon.sshash >> sshash.benchmarking_log
+./sshash bench -i cod.k47.canon.sshash >> sshash.benchmarking_log
+./sshash bench -i cod.k63.canon.sshash >> sshash.benchmarking_log
+
+./sshash bench -i kestrel.k31.canon.sshash >> sshash.benchmarking_log
+./sshash bench -i kestrel.k47.canon.sshash >> sshash.benchmarking_log
+./sshash bench -i kestrel.k63.canon.sshash >> sshash.benchmarking_log
+
+./sshash bench -i human.k31.canon.sshash >> sshash.benchmarking_log
+./sshash bench -i human.k47.canon.sshash >> sshash.benchmarking_log
+./sshash bench -i human.k63.canon.sshash >> sshash.benchmarking_log
