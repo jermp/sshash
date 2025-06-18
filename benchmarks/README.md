366676ea6cdf92dde2cab6a0d99e55072ca9f47c
Giulio Ermanno Pibiri <jeis90@gmail.com>
2025-06-18 19:09:50 +0200

    results taken on 18/06/25

--- benchmarks/README.md
+++ benchmarks/README.md
@@ -3,35 +3,32 @@
 Benchmarks
 ----------
 
-For these benchmarks we used the whole genomes
-of the following organisms:
+For these benchmarks we used the whole genomes of the following organisms:
 
-- C. Elegans ("elegans")
-- Gadus Morhua ("cod")
-- Falco Tinnunculus ("kestrel")
-- Homo Sapiens ("human")
+- Gadus Morhua ("Cod")
+- Falco Tinnunculus ("Kestrel")
+- Homo Sapiens ("Human")
 
-for k = 31, 47, and 63.
+for k = 31 and 63.
 
-The datasets can be downloaded from [Zenodo](https://zenodo.org/record/7239205).
+The datasets and queries used in these benchmarks can be downloaded
+by running the script
 
-For streaming queries we used FASTQ files downloaded from [ENA](https://www.ebi.ac.uk/ena/browser/home), using accession numbers:
+```
+bash donwload-datasets.sh
+```
 
-- elegans: SRR16288382
-- cod: SRR12858649
-- kestrel: SRR11449743
-- human: SRR5833294
+To run the benchmarks, from within the `build` directory, run
 
-The query times are relative to the following configuration:
+```
+bash ../script/build.sh [prefix]
+bash ../script/bench.sh [prefix]
+bash ../script/streaming-query-high-hit.sh [prefix]
+bash ../script/streaming-query-low-hit.sh [prefix]
+```
 
-- Processor: Intel i9-9900K @ 3.60 GHz;
-- Compiler: gcc 11.2.0;
-- OS: GNU/Linux 5.13.0-52-generic x86_64.
+where `[prefix]` should be replaced by a suitable basename, e.g., the current date.
 
-### Space in bits/kmer
+These are the results obtained on 18/06/25 (see logs [here](results-18-06-25)).
 
-| Dictionary |elegans ||| cod   ||| kestrel ||| human |||
-|:------------------|:------:|:------:|:------:|:------:|:------:|:------:|:------:|:------:|:------:|:-----:|:-------:|:-----:|
-|                   | k=31 | k=47 | k=63 | k=31 | k=47 | k=63 | k=31 | k=47 | k=63 | k=31 | k=47 | k=63 |
-| SSHash, **regular**   | 5.86 | 4.29 | 3.51 | 7.84 | 5.17 | 4.26 | 7.53 | 4.67 | 3.76 | 8.70 | 5.65 | 4.64 |
-| SSHash, **canonical** | 6.70 | 4.85 | 3.93 | 8.80 | 5.81 | 4.83 | 8.51 | 5.32 | 4.24 | 9.80 | 6.51 | 5.33 |
+![](results-18-06-25/results.png)
--- script/bench.sh
+++ script/bench.sh
@@ -1,37 +1,25 @@
 #!/bin/bash
 
+echo "output log file =" $1
+
 ### regular indexes
 
-./sshash bench -i celegans.k31.sshash >> sshash.regular.bench_log
-./sshash bench -i celegans.k47.sshash >> sshash.regular.bench_log
-./sshash bench -i celegans.k63.sshash >> sshash.regular.bench_log
+./sshash bench -i cod.k31.sshash >> $1.regular.bench_log
+# ./sshash bench -i cod.k63.sshash >> $1.regular.bench_log
 
-./sshash bench -i cod.k31.sshash >> sshash.regular.bench_log
-./sshash bench -i cod.k47.sshash >> sshash.regular.bench_log
-./sshash bench -i cod.k63.sshash >> sshash.regular.bench_log
+./sshash bench -i kestrel.k31.sshash >> $1.regular.bench_log
+# ./sshash bench -i kestrel.k63.sshash >> $1.regular.bench_log
 
-./sshash bench -i kestrel.k31.sshash >> sshash.regular.bench_log
-./sshash bench -i kestrel.k47.sshash >> sshash.regular.bench_log
-./sshash bench -i kestrel.k63.sshash >> sshash.regular.bench_log
-
-./sshash bench -i human.k31.sshash >> sshash.regular.bench_log
-./sshash bench -i human.k47.sshash >> sshash.regular.bench_log
-./sshash bench -i human.k63.sshash >> sshash.regular.bench_log
+./sshash bench -i human.k31.sshash >> $1.regular.bench_log
+# ./sshash bench -i human.k63.sshash >> $1.regular.bench_log
 
 ### canonical indexes
 
-./sshash bench -i celegans.k31.canon.sshash >> sshash.canon.bench_log
-./sshash bench -i celegans.k47.canon.sshash >> sshash.canon.bench_log
-./sshash bench -i celegans.k63.canon.sshash >> sshash.canon.bench_log
+./sshash bench -i cod.k31.canon.sshash >> $1.canon.bench_log
+# ./sshash bench -i cod.k63.canon.sshash >> $1.canon.bench_log
 
-./sshash bench -i cod.k31.canon.sshash >> sshash.canon.bench_log
-./sshash bench -i cod.k47.canon.sshash >> sshash.canon.bench_log
-./sshash bench -i cod.k63.canon.sshash >> sshash.canon.bench_log
+./sshash bench -i kestrel.k31.canon.sshash >> $1.canon.bench_log
+# ./sshash bench -i kestrel.k63.canon.sshash >> $1.canon.bench_log
 
-./sshash bench -i kestrel.k31.canon.sshash >> sshash.canon.bench_log
-./sshash bench -i kestrel.k47.canon.sshash >> sshash.canon.bench_log
-./sshash bench -i kestrel.k63.canon.sshash >> sshash.canon.bench_log
-
-./sshash bench -i human.k31.canon.sshash >> sshash.canon.bench_log
-./sshash bench -i human.k47.canon.sshash >> sshash.canon.bench_log
-./sshash bench -i human.k63.canon.sshash >> sshash.canon.bench_log
+./sshash bench -i human.k31.canon.sshash >> $1.canon.bench_log
+# ./sshash bench -i human.k63.canon.sshash >> $1.canon.bench_log
