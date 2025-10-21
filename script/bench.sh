d5987b2c85b118747caf26b0ccd330c27ce52997
Giulio Ermanno Pibiri <jeis90@gmail.com>
2025-10-21 21:37:04 +0200

    build and bench scripts updated

--- script/bench.sh
+++ script/bench.sh
@@ -21,6 +21,12 @@ done
 for i in {1..3}; do
     ./sshash bench -i hprc.k31.sshash >> results-$1/k31/regular-bench.log
 done
+for i in {1..3}; do
+    ./sshash bench -i ec.k31.sshash >> results-$1/k31/regular-bench.log
+done
+for i in {1..3}; do
+    ./sshash bench -i se.k31.sshash >> results-$1/k31/regular-bench.log
+done
 
 for i in {1..3}; do
     ./sshash bench -i cod.k31.canon.sshash >> results-$1/k31/canon-bench.log
@@ -33,6 +39,12 @@ for i in {1..3}; do
 done
 for i in {1..3}; do
     ./sshash bench -i hprc.k31.canon.sshash >> results-$1/k31/canon-bench.log
+done
+for i in {1..3}; do
+    ./sshash bench -i ec.k31.canon.sshash >> results-$1/k31/canon-bench.log
+done
+for i in {1..3}; do
+    ./sshash bench -i se.k31.canon.sshash >> results-$1/k31/canon-bench.log
 done
 
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DSSHASH_USE_ARCH_NATIVE=On -DSSHASH_USE_SANITIZERS=Off -DSSHASH_USE_MAX_KMER_LENGTH_63=On
@@ -50,6 +62,12 @@ done
 for i in {1..3}; do
     ./sshash bench -i hprc.k63.sshash >> results-$1/k63/regular-bench.log
 done
+for i in {1..3}; do
+    ./sshash bench -i ec.k63.sshash >> results-$1/k63/regular-bench.log
+done
+for i in {1..3}; do
+    ./sshash bench -i se.k63.sshash >> results-$1/k63/regular-bench.log
+done
 
 for i in {1..3}; do
     ./sshash bench -i cod.k63.canon.sshash >> results-$1/k63/canon-bench.log
@@ -62,6 +80,12 @@ for i in {1..3}; do
 done
 for i in {1..3}; do
     ./sshash bench -i hprc.k63.canon.sshash >> results-$1/k63/canon-bench.log
+done
+for i in {1..3}; do
+    ./sshash bench -i ec.k63.canon.sshash >> results-$1/k63/canon-bench.log
+done
+for i in {1..3}; do
+    ./sshash bench -i se.k63.canon.sshash >> results-$1/k63/canon-bench.log
 done
 
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DSSHASH_USE_ARCH_NATIVE=On -DSSHASH_USE_SANITIZERS=Off -DSSHASH_USE_MAX_KMER_LENGTH_63=Off
