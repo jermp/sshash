e9a525de28aa4429f16ccd76fcf418cdcf27d8b1
Giulio Ermanno Pibiri <jeis90@gmail.com>
2025-10-25 16:32:44 +0200

    simplified file_merging_iterator

--- include/builder/file_merging_iterator.hpp
+++ include/builder/file_merging_iterator.hpp
@@ -11,38 +11,33 @@ struct file_merging_iterator  //
 
     template <typename FileNamesIterator>
     file_merging_iterator(FileNamesIterator file_names_iterator, uint64_t num_files_to_merge)
-        : m_mm_files(num_files_to_merge) {
+        : m_mm_files(num_files_to_merge)  //
+    {
         if (num_files_to_merge == 0) return;
-        assert(num_files_to_merge > 0);
-
-        m_min_idx = 0;
-        m_iterators.reserve(num_files_to_merge);
-        m_idx_heap.reserve(num_files_to_merge);
 
         /* create the input iterators and make the heap */
+        m_iterators.reserve(num_files_to_merge);
         for (uint64_t i = 0; i != num_files_to_merge; ++i, ++file_names_iterator) {
             m_mm_files[i].open(*file_names_iterator, mm::advice::sequential);
             m_iterators.push_back(
                 {m_mm_files[i].data(), m_mm_files[i].data() + m_mm_files[i].size()});
-            m_idx_heap.push_back(i);
         }
 
-        if (num_files_to_merge <= scan_threshold) {
+        m_min_idx = 0;
+        if (m_iterators.size() <= scan_threshold) {
             compute_min();
         } else {
-            std::make_heap(m_idx_heap.begin(), m_idx_heap.end(), heap_idx_comparator);
-            m_min_idx = m_idx_heap.front();
+            std::make_heap(m_iterators.begin(), m_iterators.end(), heap_comparator);
         }
     }
 
-    bool has_next() { return !m_idx_heap.empty(); }
+    bool has_next() { return m_iterators.size() != 0; }
     void next() { advance_heap_head(); }
     T operator*() const { return *(m_iterators[m_min_idx].begin); }
 
     void close() {
         for (uint64_t i = 0; i != m_mm_files.size(); ++i) m_mm_files[i].close();
         m_iterators.clear();
-        m_idx_heap.clear();
         m_mm_files.clear();
     }
 
@@ -52,44 +47,38 @@ private:
         T const* end;
     };
     std::vector<pointer_pair> m_iterators;
-    std::vector<uint32_t> m_idx_heap;
     std::vector<mm::file_source<T const>> m_mm_files;
     uint64_t m_min_idx;
 
-    std::function<bool(uint32_t, uint32_t)> heap_idx_comparator = [&](uint32_t i, uint32_t j) {
-        assert(i < m_iterators.size() and j < m_iterators.size());
-        assert(m_iterators[i].begin != m_iterators[i].end and
-               m_iterators[j].begin != m_iterators[j].end);
-        return *(m_iterators[i].begin) > *(m_iterators[j].begin);
-    };
+    std::function<bool(pointer_pair, pointer_pair)> heap_comparator =
+        [](pointer_pair x, pointer_pair y) { return *(x.begin) > *(y.begin); };
 
     void advance_heap_head() {
         auto& it = m_iterators[m_min_idx];
         it.begin += 1;
-        if (m_iterators.size() <= scan_threshold) {  // linear scan
+        if (m_iterators.size() <= scan_threshold) {  // compute min with a linear scan
             if (it.begin == it.end) {
                 m_iterators.erase(m_iterators.begin() + m_min_idx);
-                m_idx_heap.pop_back();
                 m_min_idx = 0;
-                if (m_iterators.empty()) return;
+                if (m_iterators.size() == 0) return;
             }
             compute_min();
-        } else {                       // min-heap code
-            if (it.begin != it.end) {  // if iterator has next
+        } else {  // update the min-heap
+            if (it.begin != it.end) {
                 uint64_t pos = 0;
-                uint64_t size = m_idx_heap.size();
+                uint64_t size = m_iterators.size();
                 while (2 * pos + 1 < size) {
                     uint64_t i = 2 * pos + 1;
-                    if (i + 1 < size and heap_idx_comparator(m_idx_heap[i], m_idx_heap[i + 1])) ++i;
-                    if (heap_idx_comparator(m_idx_heap[i], m_idx_heap[pos])) break;
-                    std::swap(m_idx_heap[pos], m_idx_heap[i]);
+                    if (i + 1 < size and heap_comparator(m_iterators[i], m_iterators[i + 1])) ++i;
+                    if (heap_comparator(m_iterators[i], m_iterators[pos])) break;
+                    std::swap(m_iterators[pos], m_iterators[i]);
                     pos = i;
                 }
             } else {
-                std::pop_heap(m_idx_heap.begin(), m_idx_heap.end(), heap_idx_comparator);
-                m_idx_heap.pop_back();
+                std::pop_heap(m_iterators.begin(), m_iterators.end(), heap_comparator);
+                m_iterators.pop_back();
             }
-            m_min_idx = m_idx_heap.front();
+            assert(m_min_idx == 0);
         }
     };
 
