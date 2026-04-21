1f0cdd16e996df7d373c5a08a44da08b4338bfe7
Giulio Ermanno Pibiri <jeis90@gmail.com>
2026-04-21 21:28:28 +0200

    fixed issue with minimizers_tuples_iterator

--- include/builder/util.hpp
+++ include/builder/util.hpp
@@ -146,13 +146,10 @@ private:
     minimizer_tuple const* m_end;
 
     minimizer_tuple const* next_begin() {
+        if (m_bucket_begin == m_end) return m_end;
         minimizer_tuple const* begin = m_bucket_begin;
-        uint64_t prev_minimizer = (*begin).minimizer;
-        while (begin != m_end) {
-            ++begin;
-            uint64_t curr_minimizer = (*begin).minimizer;
-            if (curr_minimizer != prev_minimizer) break;
-        }
+        uint64_t prev_minimizer = begin->minimizer;
+        while (++begin != m_end and begin->minimizer == prev_minimizer) {}
         return begin;
     }
 };
